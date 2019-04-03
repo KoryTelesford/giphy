73802a634c31be176d35921e30f7ce2873c7833d
Kory Telesford <tkory85@gmail.com>
2019-04-03 21:57:44 +0000

    JAVASCRIPT WORKSgit add .git add .git add .!

--- js/script.js
+++ js/script.js
@@ -4,8 +4,24 @@
 /* global $ */
 
 $("#search-button").click(function(){
+  var search = $("input").val();
   
+  console.log(search);
   
+  var request_url = "https://api.giphy.com/v1/gifs/search?q=" + search + "&rating=pg&api_key=dc6zaTOxFJmzC";
+  
+  $.ajax({
+        url: request_url,
+        method: "GET",
+        success: function(response) {
+            // YOUR CODE GOES HERE
+            $('body').append("<img src=" + response.data[0].images.original.url + ">");
+            
+            
+        },
+    }); 
   
 });
 
+
+    
